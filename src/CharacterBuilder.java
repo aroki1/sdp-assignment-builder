86cf28070e6fc8146773648ac032103e5857fdb4
aroki1 <135426318+aroki1@users.noreply.github.com>
2026-09-09 14:15:09 +0600

    Add CharacterBuilder interface

---
+++ src/CharacterBuilder.java
@@ -0,0 +1,6 @@
+public interface CharacterBuilder {
+    CharacterBuilder setName(String name);
+    CharacterBuilder setRole(String role);
+    CharacterBuilder setWeapon(String weapon);
+    CharacterBuilder setLevel(int level);
+}
